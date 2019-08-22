c12a317430061d2a4560232fb55168f33ef18680
Alumno <Alumno@PC10>
2019-08-22 09:15:21 -0500

    xddd

--- CicloFor/CicloFor/Program.cs
+++ CicloFor/CicloFor/Program.cs
@@ -6,7 +6,32 @@ namespace CicloFor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x,y,z;
+            Console.WriteLine("Bienvenido a mi programa");
+
+            Console.WriteLine("Dime el primer numero");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dime el segundo numero");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dime de cuanto en cuanto quieres que vaya");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if(x>y)
+            {
+                for (int i=x;i>y;i-=z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            else
+            {
+                for (int i=x;i<y;i+=z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
         }
     }
 }
